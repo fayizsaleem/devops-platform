afc4643bba419e209b9c09aae0ae28b23a1103ec
fayizsaleem <fayizsaleem@gmail.com>
2025-12-10 13:22:34 +0000

    ff

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,51 +2,60 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 3.0"
+      version = "3.6.2"
     }
   }
 }
 
 provider "docker" {}
 
-resource "docker_network" "app_net" {
+########################################
+# 🔹 NETWORK (Creates only if missing)
+########################################
+
+data "docker_network" "existing" {
   name = "devops_app_network"
-
-  check_duplicate = true
 }
 
-resource "docker_image" "user_image" {
-  name = "localhost:5001/user-service:latest"
+resource "docker_network" "app_net" {
+  count = data.docker_network.existing.id == "" ? 1 : 0
+  name  = "devops_app_network"
 }
 
-resource "docker_image" "order_image" {
-  name = "localhost:5001/order-service:latest"
-}
+########################################
+# 🔹 USER SERVICE CONTAINER (Python)
+########################################
 
 resource "docker_container" "user_service" {
   name  = "user-service"
-  image = docker_image.user_image.name
-
-  networks_advanced {
-    name = docker_network.app_net.name
-  }
+  image = "localhost:5001/user-service:latest"
+  restart = "always"
 
   ports {
     internal = 5000
     external = 5000
   }
+
+  networks_advanced {
+    name = "devops_app_network"
+  }
 }
+
+########################################
+# 🔹 ORDER SERVICE CONTAINER (NodeJS)
+########################################
 
 resource "docker_container" "order_service" {
   name  = "order-service"
-  image = docker_image.order_image.name
-
-  networks_advanced {
-    name = docker_network.app_net.name
-  }
+  image = "localhost:5001/order-service:latest"
+  restart = "always"
 
   ports {
     internal = 4000
     external = 4000
+  }
+
+  networks_advanced {
+    name = "devops_app_network"
   }
 }
